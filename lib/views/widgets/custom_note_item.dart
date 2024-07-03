0645ade4c254aba90d15da5982c6755c9d1dca0b
ali.jalo <admirralty@gmail.com>
2024-07-03 21:00:49 +0300

    create note item and added font

---
+++ lib/views/widgets/custom_note_item.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(top: 24, bottom: 24),
+      decoration: BoxDecoration(
+        color: const Color.fromARGB(255, 214, 175, 18),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: const Text(
+              'Flutter Tips',
+              style: TextStyle(color: Colors.black, fontSize: 24),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16),
+              child: Text(
+                'Build your career with us Build your career with ',
+                style: TextStyle(
+                    color: Colors.black.withOpacity(0.6), fontSize: 20),
+              ),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(
+                FontAwesomeIcons.trash,
+                color: Colors.black,
+                size: 24,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 24),
+            child: Text(
+              'Jul03, 2024',
+              style: TextStyle(color: Colors.black.withOpacity(0.6)),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
