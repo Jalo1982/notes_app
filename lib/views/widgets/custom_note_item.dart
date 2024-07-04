844963a5c2ad9ba06a6390ea757885b7ed657f83
ali.jalo <admirralty@gmail.com>
2024-07-04 12:46:49 +0300

    add floating button

--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -30,7 +30,7 @@ class NoteItem extends StatelessWidget {
             ),
             trailing: IconButton(
               onPressed: () {},
-              icon: Icon(
+              icon: const Icon(
                 FontAwesomeIcons.trash,
                 color: Colors.black,
                 size: 24,
