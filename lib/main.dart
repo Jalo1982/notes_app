611a22a8296cd385b0081fe404b9354c644ae68e
ali.jalo <admirralty@gmail.com>
2024-07-06 14:41:21 +0300

    Provide add note cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:notes_app/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/constant.dart';
 import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/notes_view.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
   await Hive.initFlutter();
@@ -17,12 +19,14 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        fontFamily: 'RobotoSlab',
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => AddNoteCubit()),
+      ],
+      child: MaterialApp(
+        theme: ThemeData(brightness: Brightness.dark, fontFamily: 'RobotoSlab'),
+        home: const NotesView(),
       ),
-      home: const NotesView(),
     );
   }
 }
