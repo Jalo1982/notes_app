ff383096009a7368124d4e29d8920fb0a74a4fe4
ali.jalo <admirralty@gmail.com>
2024-07-06 19:40:11 +0300

    Make custom button active

--- lib/views/widgets/custom_button.dart
+++ lib/views/widgets/custom_button.dart
@@ -14,16 +14,27 @@ class CustomButton extends StatelessWidget {
         width: MediaQuery.of(context).size.width,
         height: 55,
         decoration: BoxDecoration(
-            color: kPrimaryColor, borderRadius: BorderRadius.circular(16)),
-        child: const Center(
-          child: Text(
-            'Save',
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
+            color: kPrimaryColor,
+            borderRadius: BorderRadius.circular(
+              8,
+            )),
+        child: Center(
+          child: isLoading
+              ? const SizedBox(
+                  height: 24,
+                  width: 24,
+                  child: CircularProgressIndicator(
+                    color: Colors.black,
+                  ),
+                )
+              : const Text(
+                  'Add',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
         ),
       ),
     );
