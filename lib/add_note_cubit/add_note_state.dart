873a6f07766c5b08544668afe625d579a276591d
ali.jalo <admirralty@gmail.com>
2024-07-06 12:53:18 +0300

    Add note state

--- lib/add_note_cubit/add_note_state.dart
+++ lib/add_note_cubit/add_note_state.dart
@@ -1,6 +1,26 @@
 part of 'add_note_cubit.dart';
 
 @immutable
-sealed class NotesState {}
+abstract class AddNoteState {}
 
-final class NotesInitial extends NotesState {}
+class AddNoteInitial extends AddNoteState {}
+
+class AddNoteLoading extends AddNoteState {}
+
+class AddNoteSuccess extends AddNoteState {}
+
+class AddNoteFailure extends AddNoteState {
+  final String errMessage;
+  AddNoteFailure(this.errMessage);
+}
+
+
+
+
+
+
+// final class NotesInitial extends NotesState {}
+// final class NotesLoading extends NotesState {}
+// final class NotesSuccess extends NotesState {}
+// final class NotesFailure extends NotesState {}
+// final class NoteserrMessage extends NotesState {}
